e6745d91f0c79365957be9edb8407bcfc926593e
Armemius <rusgy@yandex.ru>
2025-01-06 06:05:50 +0300

    [~] Change formatting

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 from q_learning import QLearning
 
+
 def main():
     q = QLearning()
-    q.train(episodes=100000)
+    q.train(episodes=300000)
     q.plot()
     q.q_table.dump_csv()
     q.test()
+
 
 if __name__ == "__main__":
     main()
